808745693895a5febaf37aff9ce6210dc6b334d1
sheila <sheilavp_a96@hotmail.com>
2023-09-12 18:12:20 -0600

    separando por componentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./styles.css";
+import { tempMovieData, tempWatchedData } from "./dummyData/dummyData";
+import { Navbar } from "./components/Navbar";
+import { MoviesList } from "./components/MoviesList";
+import { MoviesWatched } from "./components/MoviesWatched";
 
-function App() {
+const average = (arr) => arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+
+export default function App() {
+  const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
+  const [isOpen1, setIsOpen1] = useState(true);
+  const [isOpen2, setIsOpen2] = useState(true);
+
+  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
+  const avgUserRating = average(watched.map((movie) => movie.userRating));
+  const avgRuntime = average(watched.map((movie) => movie.runtime));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar movies={movies} />
+      <main className="main">
+        <div className="box">
+          <button className="btn-toggle" onClick={() => setIsOpen1((open) => !open)}>
+            {isOpen1 ? "–" : "+"}
+          </button>
+          {isOpen1 && <MoviesList movies={movies} />}
+        </div>
+
+        <div className="box">
+          <button className="btn-toggle" onClick={() => setIsOpen2((open) => !open)}>
+            {isOpen2 ? "–" : "+"}
+          </button>
+          {isOpen2 && (
+            <MoviesWatched
+              watched={watched}
+              avgImdbRating={avgImdbRating}
+              avgUserRating={avgUserRating}
+              avgRuntime={avgRuntime}
+            />
+          )}
+        </div>
+      </main>
+    </>
   );
 }
-
-export default App;
