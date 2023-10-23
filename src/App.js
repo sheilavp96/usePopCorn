d4a6d09934259fdf47ad8e8a3e6f27386e1dd0cf
sheila <sheilavp_a96@hotmail.com>
2023-10-22 19:45:09 -0600

    Conexión con la api de peliculas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { useState } from "react";
-// import "./styles.css";
+import { useState, useEffect } from "react";
+import "./styles.css";
 import { tempMovieData, tempWatchedData } from "./dummyData/dummyData";
 import { Navbar } from "./components/Navbar";
 import { Main } from "./components/Main";
@@ -9,25 +9,61 @@ import { Search } from "./components/Search";
 import { Box } from "./components/Box";
 import { MovieList } from "./components/MovieList";
 import { Watch } from "./components/Watch";
-import { StartRating } from "./components/StartRating";
-import { Expander } from "./expander/Expander";
+import Loader from "./components/Loader";
+import ErrorMessage from "./components/ErrorMessage";
+// import { StartRating } from "./components/StartRating";
+// import { Expander } from "./expander/Expander";
 
+const key = "f84fc31d";
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
   const [watched, setWatched] = useState(tempWatchedData);
+  const query = "asuhujhuhuh";
+  useEffect(() => {
+    setIsLoading(true);
+    async function fetchMovie() {
+      try {
+        const res = await fetch(`http://www.omdbapi.com/?apikey=${key}&s=${query}`);
+        const data = await res.json();
+        if (data.Response === "False") {
+          throw new Error(data.Error);
+        }
+        setMovies(data.Search);
+      } catch (e) {
+        console.log(e, e.message);
+        setError(e.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchMovie();
+  }, []);
 
   return (
     <>
-      {/* <Navbar>
+      <Navbar>
         <Logo />
         <Search />
         <Results movies={movies} />
       </Navbar>
       <Main>
-        <Box element={<MovieList movies={movies} />} />
+        <Box
+          element={
+            isLoading ? (
+              <Loader />
+            ) : !isLoading && !error ? (
+              <MovieList movies={movies} />
+            ) : (
+              <ErrorMessage message={error} />
+            )
+          }
+        />
         <Box element={<Watch watched={watched} />} />
-      </Main> */}
-      <Expander />
+      </Main>
+      {/* <Expander /> */}
       {/* <StartRating message={["Terrible", "Bad", "Okay", "Good", "Amazing"]} /> */}
       {/* <StartRating color="pink" maxRating={10} /> */}
     </>
---
+++ src/components/ErrorMessage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ErrorMessage({ message }) {
+  return <h1>⛔ {message}</h1>;
+}
